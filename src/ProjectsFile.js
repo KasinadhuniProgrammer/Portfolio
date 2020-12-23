5ea7e2c64049cdef37f5f51a6cbaa3a1369a60c3
prodSatvikK <rachitipidudraksha@gmail.com>
2020-12-22 19:58:41 -0500

    updated portfolio

--- src/ProjectsFile.js
+++ src/ProjectsFile.js
@@ -16,7 +16,7 @@ function ProjectsFiles() {
                     <center><button onClick={() => {openInNewTab("https://devpost.com/software/satviks-slack")}} className="satviksslackprojectbutton">
                         <center><img src="https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_photos/001/317/785/datas/medium.png" alt=""/></center>
                         <center><h1>Satviks Slack</h1></center>
-                        <center><h3>My own version of Slack!</h3></center>
+                        <center><h3>My own version of Slack! I developed this with React.js and Firebase. Click here to be directed to its devpost!</h3></center>
                     </button></center>
                 </div>
             </div>
