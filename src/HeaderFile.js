593f50a0ff761597cd6447f6f40c318adbf2f0bc
prodSatvikK <rachitipidudraksha@gmail.com>
2020-12-23 07:29:30 -0500

    updated portfolio again

--- src/HeaderFile.js
+++ src/HeaderFile.js
@@ -13,8 +13,8 @@ function HeaderFile() {
             <center><img className="myimg" src="https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/001/121/880/datas/xlarge.png" alt=""/></center>
             <h1 className="mename"><center>Satvik Kasinadhuni</center></h1>
             <h1 className="meranddescription"><center>High School Student | Full Stack Developer</center></h1>
-            <center><button onClick={() => {openInNewTab("https://github.com/KasinadhuniProgrammer")}} className="github-button">GitHub</button></center>
-            <center><button onClick={() => {openInNewTab("https://www.linkedin.com/in/satvik-kasinadhuni/")}}className="linkedin-button">Linkedin</button></center>
+            <center><button onClick={() => {openInNewTab("https://github.com/KasinadhuniProgrammer")}} className="github-button"><h5>GitHub</h5></button></center>
+            <center><button onClick={() => {openInNewTab("https://www.linkedin.com/in/satvik-kasinadhuni/")}}className="linkedin-button"><h5>Linkedin</h5></button></center>
 
         </div>
     ); 
