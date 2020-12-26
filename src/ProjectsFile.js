c9b787ba5f5d194c2ec441cb4e69b55f9411967e
prodSatvikK <rachitipidudraksha@gmail.com>
2020-12-25 22:01:04 -0500

    updated project section on website

--- src/ProjectsFile.js
+++ src/ProjectsFile.js
@@ -16,8 +16,18 @@ function ProjectsFiles() {
             <div className="satviksslackprojectthing">
                 <div className="satviksslackprojectthing__container">
                     <center><h2>Satviks Slack</h2></center>
-                    <center><h3>My own version of Slack, built with React.js and Firebase.</h3></center>
+                    <center><h3>December 2020</h3></center>
+                    <center><h4>My own version of Slack, built with React.js and Firebase.</h4></center>
 
+                </div>
+                <div className="mechlevelsprojectthing">
+                    <div className="mechlevelsprojectthing__container">
+                        <center><h2>MechLevels</h2></center>
+                        <center><h3>December 2020 - PRESENT</h3></center>
+                        <center><h4>A website for Mechanical Engineers to see compensations from top companies.</h4></center>
+
+                    </div>
+                    
                 </div>
             </div>
             
