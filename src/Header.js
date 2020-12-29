17c98984f7314125c58aef0315cfe243b26da12d
prodSatvikK <rachitipidudraksha@gmail.com>
2020-12-29 16:47:41 -0500

    finished cool website redesign

--- src/Header.js
+++ src/Header.js
@@ -16,7 +16,7 @@ function Header() {
         <div className="header">
             <center><img className="header__imageh" src={FRESHMANYEARFIRSTIMAGE}/></center>
             <center><h1 className="header__nameh">Satvik Kasinadhuni</h1></center>
-            <center><h3 className="header__desch">Student | Frontend Developer</h3></center>
+            <center><h3 className="header__desch">Student | Full Stack Developer</h3></center>
             <GitHubIcon className="header__github-icon" onClick={() => {openInNewTab("https://github.com/KasinadhuniProgrammer")}}/>
             <LinkedInIcon className="header__linkedin-icon" onClick={() => {openInNewTab("https://www.linkedin.com/in/satvik-kasinadhuni")}}/>
             <TwitterIcon className="header__twitter-icon" onClick={() => {openInNewTab("https://twitter.com/ChubbyIndianBoy")}}/>
--- src/Technical.js
+++ src/Technical.js
@@ -5,6 +5,7 @@ function Technical() {
     return (
         <div className="technical">
             <center><h1>Thank you, and best of luck!</h1></center>
+            <center><h3>skasinad@gmail.com</h3></center>
         </div>
     );
 
