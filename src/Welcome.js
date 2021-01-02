3d03c699fc944b3213c210ca0103af413f33498e
prodSatvikK <rachitipidudraksha@gmail.com>
2021-01-02 10:44:02 -0500

    edited intro section of portfolio

--- src/Welcome.js
+++ src/Welcome.js
@@ -4,9 +4,11 @@ import './Welcome.css';
 function Welcome() {
     return(
         <div className="welcome">
-            <center><h1>Hey There! 👋</h1></center>
-            <center><h3>My name is Satvik Kasinadhuni, I am currently a High School Sophomore 👨‍🎓, and a Software Developer 💻! I am very passionate for Software Development.</h3></center>
-            <center><h3>Apart from coding, in my free time, I like to play video games 🎮, watch tv 📺, and go biking 🚲!</h3></center>
+            <center><h1>Introduction 👋</h1></center>
+            <center><h3>My programming journey started off in 2015, when I was 10 years old. I wanted to create a small rock paper scissors game so that my brother and I can play a self made game with our computer. I started with learning Python and I eventually made my rock papers scissors game and enjoyed playing around with it.</h3></center>
+            <center><h3>I then started to learn more about Python, and I developed applications with things such as Turtle and Tkinter. I started to find a spark in myself when I was programming. Programming soon started to become a passion of mine.</h3></center>
+            <center><h3>Since then, I have been programming a lot, I am part of a robotics team and I code in Java over there for the autonomous programs of the robot. I even picked up Full Stack Development and learned things such as ReactJS and Firebase, and built cool full stack projects.</h3></center>
+            <center><h3>Now, I am currently programming way more and I am constantly learning new things, to become the best programmer I can be!</h3></center>
 
         </div>
     );
