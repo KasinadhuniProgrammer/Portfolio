890ee69b0f9a7a20ecdf5a3457f497b84f73bac2
prodSatvikK <rachitipidudraksha@gmail.com>
2021-01-02 22:31:13 -0500

    another typo, fixed :)

--- src/Header.js
+++ src/Header.js
@@ -27,7 +27,7 @@ function Header() {
           <span>I am a High School Student</span>
           <Typing.Delay ms={1000} />
           <Typing.Backspace count={64} speed={5}/>
-          <span>I am based in the U.S.A</span>
+          <span>I am based in the U.S.</span>
           <Typing.Delay ms={1000} />
           <Typing.Backspace count={64} speed={5}/>
           <span>Welcome to my portfolio!</span>
