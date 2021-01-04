981cbbda1ff5a6a20742c68754befbe23452c435
prodSatvikK <rachitipidudraksha@gmail.com>
2021-01-04 07:16:59 -0500

    changing speed of text header animation

--- src/Header.js
+++ src/Header.js
@@ -27,7 +27,7 @@ function Header() {
           <span>I am a High School Student</span>
           <Typing.Delay ms={1000} />
           <Typing.Backspace count={64} speed={5}/>
-          <span>I am an aspiring Ethical Hacker</span>
+          <span>I love Cyber Security</span>
           <Typing.Delay ms={1000} />
           <Typing.Backspace count={64} speed={5}/>
           <span>Welcome to my portfolio!</span>
