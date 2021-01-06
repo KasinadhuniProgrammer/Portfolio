e168eb32bdc5bf312d71b3e3277d17173016e1a1
prodSatvikK <rachitipidudraksha@gmail.com>
2021-01-06 15:23:53 -0500

    editing work experience

--- src/WorkExperience.js
+++ src/WorkExperience.js
@@ -10,11 +10,6 @@ function WorkExperience() {
                 <center><h3>Position: High School Intern</h3></center>
                 <center><h3>Duration: JUNE 2020 - NOVEMBER 2020</h3></center>
             </div>
-            <div className="workexperience__secondcontainer">
-                <center><h3>Company Name: TRUMPF</h3></center>
-                <center><h3>Position: Production Engineering Externship</h3></center>
-                <center><h3>Duration: February 2020</h3></center>
-            </div>
         </div>
     );
 }
