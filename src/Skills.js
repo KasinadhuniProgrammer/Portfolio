19eb67c7b081a267ef3c7e6d80d089dc7fde9fbc
prodSatvikK <rachitipidudraksha@gmail.com>
2021-01-11 18:25:40 -0500

    updated skills section

--- src/Skills.js
+++ src/Skills.js
@@ -7,9 +7,9 @@ function Skills() {
     return (
         <div className="skills">
             <center><h1>Skills</h1></center>
-            <center><h3>Proficient: React.js, JavaScript, C++, Java, Git, Firebase, Object-Oriented Programming, Algorithms, Data Structures, Microsoft Excel, Microsoft Word, Microsoft PowerPoint</h3></center>
-            <center><h3>Familiar: Python, C#, Figma, Autodesk Inventor, TensorFlow.js, Linux</h3></center>
-            <center><h3>Novice: Django, AWS, Docker</h3></center>
+            <center><h3>Proficient: React.js, JavaScript, Python, Tkinter, Java, Git, Firebase, Object-Oriented Programming, Algorithms, Data Structures, Microsoft Excel, Microsoft Word, Microsoft PowerPoint</h3></center>
+            <center><h3>Familiar: C++, C#, Figma, Autodesk Inventor, TensorFlow.js, Linux</h3></center>
+            <center><h3>Novice: Django, Docker</h3></center>
 
         </div>
     );
