37ba40f1db7ae3be4593795b9e4b81f823425ca1
prodSatvikK <rachitipidudraksha@gmail.com>
2021-01-11 21:00:28 -0500

    animated header

--- src/Technical.js
+++ src/Technical.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './Technical.css';
 
+import Typing from 'react-typing-animation';
+
 function Technical() {
+    const NewAnimatedTypingComponent = () => (
+        <Typing loop={true}>
+          <span>Website coded with React.js</span>
+          <Typing.Delay ms={1000} />
+          <Typing.Backspace count={64} speed={5}/>
+          <span>Email me at skasinad@gmail.com</span>
+          <Typing.Delay ms={1000} />
+          <Typing.Backspace count={64} speed={5}/>
+          <span>Thank you, and best of luck!</span>
+          <Typing.Delay ms={5000} />
+          <Typing.Backspace count={64} speed={5}/>
+
+
+
+        </Typing>
+      );
+
+
     return (
         <div className="technical">
-            <center><h1>Thank you, and best of luck!</h1></center>
-            <center><h3>skasinad@gmail.com</h3></center>
-            <center><h3>Website coded with React.js</h3></center>
+            <center><h1><NewAnimatedTypingComponent /></h1></center>
+
+            
         </div>
     );
 
