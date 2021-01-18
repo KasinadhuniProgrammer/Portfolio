2597d648c618f0c0e868024f40bc36303f1e52e6
prodSatvikK <rachitipidudraksha@gmail.com>
2021-01-18 13:37:41 -0500

    website info updates

--- src/Header.js
+++ src/Header.js
@@ -27,7 +27,7 @@ function Header() {
           <span>I am a Software Developer</span>
           <Typing.Delay ms={1000} />
           <Typing.Backspace count={64} speed={5}/>
-          <span>I love problem solving</span>
+          <span>I am an Ethical Hacker</span>
           <Typing.Delay ms={1000} />
           <Typing.Backspace count={64} speed={5}/>
           <span>I am from the United States</span>
--- src/Welcome.js
+++ src/Welcome.js
@@ -11,7 +11,7 @@ function Welcome() {
             <center><h1>Introduction 👋</h1></center>
             <center><h3>My programming journey started off in 2015, when I was 10 years old. I wanted to create a small rock paper scissors game so that my brother and I can play a self made game with our computer. I started with learning Python and I eventually made my rock papers scissors game and enjoyed playing around with it.</h3></center>
             <center><h3>I then started to learn more about Python, and I developed applications with things such as Turtle and Tkinter. I started to find a spark in myself when I was programming. Programming soon started to become a passion of mine.</h3></center>
-            <center><h3>Since then, I have been programming a lot, I am part of a robotics team and I code in Java over there for the autonomous programs of the robot. I even picked up Full Stack Development and learned things such as ReactJS and Firebase, and built cool full stack projects.</h3></center>
+            <center><h3>Since then, I have been programming a lot, I am part of a robotics team and I code in Java over there for the autonomous programs of the robot. I even picked up Full Stack Development and learned things such as ReactJS and Firebase, and built cool full stack projects. I also got interested in Ethical Hacking and Cyber Security, and am also learning more about that as well!</h3></center>
             <center><h3>Now, I am currently programming way more and I am constantly learning new things, to become the best programmer I can be!</h3></center>
             <center><h3>I am currently looking for Software Engineering Internships for the summer.</h3></center>
 
