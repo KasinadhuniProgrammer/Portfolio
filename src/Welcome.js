f81ba10189bdf9b9abe235d664ba6ed759a00232
prodSatvikK <rachitipidudraksha@gmail.com>
2021-01-24 13:23:33 -0500

    updated intro section

--- src/Welcome.js
+++ src/Welcome.js
@@ -9,11 +9,8 @@ function Welcome() {
     return(
         <div className="welcome">
             <center><h1>Introduction 👋</h1></center>
-            <center><h3>My programming journey started off in 2015, when I was 10 years old. I wanted to create a small rock paper scissors game so that my brother and I can play a self made game with our computer. I started with learning Python and I eventually made my rock papers scissors game and enjoyed playing around with it.</h3></center>
-            <center><h3>I then started to learn more about Python, and I developed applications with things such as Turtle and Tkinter. I started to find a spark in myself when I was programming. Programming soon started to become a passion of mine.</h3></center>
-            <center><h3>Since then, I have been programming a lot, I am part of a robotics team and I code in Java over there for the autonomous programs of the robot. I even picked up Full Stack Development and learned things such as ReactJS and Firebase, and built cool full stack projects. I also got interested in Ethical Hacking and Cyber Security, and am also learning more about that as well!</h3></center>
-            <center><h3>Now, I am currently programming way more and I am constantly learning new things, to become the best programmer I can be!</h3></center>
-            <center><h3>I am currently looking for Software Engineering Internships for the summer.</h3></center>
+            <center><h3>I am Satvik, and I am a Sophomore at Farmington High School and am very passionate for Software Engineering.</h3></center>
+            <center><h3>I began getting into Software Engineering when I was 10, and I just continued to learn more about it. I mostly do a lot of Frontend using React.js, but I also have experience with Firebase, Python, Ethical Hacking, Linux, and Java.</h3></center>
 
         </div>
     );
