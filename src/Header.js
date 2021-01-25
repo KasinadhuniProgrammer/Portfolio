c7e13bf04d2e0bdecf2d996eb7999d9c380b0058
prodSatvikK <rachitipidudraksha@gmail.com>
2021-01-24 19:27:50 -0500

    updated header and introduction

--- src/Header.js
+++ src/Header.js
@@ -30,7 +30,7 @@ function Header() {
           <span>I am an Ethical Hacker</span>
           <Typing.Delay ms={1000} />
           <Typing.Backspace count={64} speed={5}/>
-          <span>I am from the United States</span>
+          <span>I love Frontend</span>
           <Typing.Delay ms={1000} />
           <Typing.Backspace count={64} speed={5}/>
           <span>Welcome to my portfolio!</span>
