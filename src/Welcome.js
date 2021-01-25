c87b249af3f6c9ae071248adbd0f793f79fa978d
prodSatvikK <rachitipidudraksha@gmail.com>
2021-01-25 16:26:29 -0500

    editing intro section

--- src/Welcome.js
+++ src/Welcome.js
@@ -11,7 +11,7 @@ function Welcome() {
             <center><h1>Introduction 👋</h1></center>
             <center><h3>I am Satvik, and I am a Sophomore at Farmington High School with a huge passion for Software Engineering.</h3></center>
             <center><h3>I began getting into Software Engineering when I was 10, and I just continued to learn more about it. I mostly do a lot of Frontend using React.js, but I also have experience with Firebase, Python, Ethical Hacking, Linux, and Java.</h3></center>
-            <center><h3>After High School, I am interested to go to college and study Computer Science. My dream is to work as a Software Engineer for Lockheed Martin.</h3></center>
+            <center><h3>After High School, I am interested to go to college and study Computer Science. Eventually I want to work for a company as a Software Engineer.</h3></center>
 
         </div>
     );
