be10a8019fc17293c04fb286d853fa4cdecd5a2a
prodSatvikK <rachitipidudraksha@gmail.com>
2021-01-24 19:30:52 -0500

    fixed grammar errors in intro section

--- src/Welcome.js
+++ src/Welcome.js
@@ -9,7 +9,7 @@ function Welcome() {
     return(
         <div className="welcome">
             <center><h1>Introduction 👋</h1></center>
-            <center><h3>I am Satvik, and I am a Sophomore at Farmington High School and am very passionate for Software Engineering.</h3></center>
+            <center><h3>I am Satvik, and I am a Sophomore at Farmington High School with a huge passion for Software Engineering.</h3></center>
             <center><h3>I began getting into Software Engineering when I was 10, and I just continued to learn more about it. I mostly do a lot of Frontend using React.js, but I also have experience with Firebase, Python, Ethical Hacking, Linux, and Java.</h3></center>
             <center><h3>After High School, I am interested to go to college and study Computer Science. My dream is to work as a Software Engineer for Lockheed Martin.</h3></center>
 
