0019c4bedd6faec8d12b762f651a57bac881aaa8
prodSatvikK <rachitipidudraksha@gmail.com>
2021-01-28 23:30:37 -0500

    fixed linked issue

--- src/Connect.js
+++ src/Connect.js
@@ -6,7 +6,7 @@ function Connect() {
     return (
         <div className="connect">
             <center><h1>Thank you, and best of luck!</h1></center>
-            <center><h3>Website created by <a href="www.satvik.site">Satvik Kasinadhuni</a></h3></center>
+            <center><h3>Website created by <a href="https://kasinadhuniprogrammer.github.io/Portfolio/">Satvik Kasinadhuni</a></h3></center>
         </div>
     );
 }
